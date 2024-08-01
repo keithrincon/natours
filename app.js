bd34b606cfe37c456cd57346355596c17e2714e6
Keith Rincon <keithrt3008@gmail.com>
2024-08-01 08:29:45 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ const viewRouter = require('./routes/viewRoutes');
 // Start express app
 
 const app = express();
+app.set('trust proxy', 1);
+app.get('/ip', (request, response) => response.send(request.ip));
+app.get('/x-forwarded-for', (request, response) =>
+  response.send(request.headers['x-forwarded-for']),
+);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
