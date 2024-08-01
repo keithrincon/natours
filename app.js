60f0b2a43a2333a927a67cea4492f1a840caf5b3
Keith Rincon <keithrt3008@gmail.com>
2024-08-01 08:54:27 -0700

    fixed bad gateway error

--- app.js
+++ app.js
@@ -7,39 +7,52 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+// const bookingRouter = require('./routes/bookingRoutes');
+// const bookingController = require('./controllers/bookingController');
+
 const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
-
 const app = express();
-app.set('trust proxy', 1);
-app.get('/ip', (request, response) => response.send(request.ip));
-app.get('/x-forwarded-for', (request, response) =>
-  response.send(request.headers['x-forwarded-for']),
-);
+
+app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
+// Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set Security HTTP headers
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+// Set security HTTP headers
+app.use(helmet());
 
-// Developming logging
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//Limit requests from same api
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -47,13 +60,21 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-//Body parser, reading data from body into req.body
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout,
+);
+
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
-// Data sanitization against NoSQL  query injection
+// Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
 // Data sanitization against XSS
 app.use(xss());
 
@@ -62,8 +83,8 @@ app.use(
   hpp({
     whitelist: [
       'duration',
-      'ratingQuantity',
-      'ratingAverage',
+      'ratingsQuantity',
+      'ratingsAverage',
       'maxGroupSize',
       'difficulty',
       'price',
@@ -81,11 +102,11 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
